e0b94fa779086d1b13c49a33901df5f8e63b2716
Lanil Marasinghe <lanil@bitbucket.org>
2021-06-16 16:59:29 +0530

    integrated handlebars template to show daily usage

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 
 const { getYesterdayValues, getTodayValues } = require("./utils/vnstatUtils");
 const { convertValuesToBytes } = require("./utils/numberUtils");
 
 const app = express();
 
-app.listen(8083, async () => {
-  console.log("VNSTAT Monitor Running");
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+app.get("/", async function (req, res) {
   try {
     const yesterdayData = convertValuesToBytes(await getYesterdayValues());
     const todayData = convertValuesToBytes(await getTodayValues());
-
-    console.log("-----------------------------------");
-    console.log(yesterdayData);
-    console.log("-----------------------------------");
-    console.log(todayData);
+    res.render("home", {
+      yesterdayReceived: yesterdayData.received,
+      yesterdaySent: yesterdayData.sent,
+      yesterdayTotal: yesterdayData.total,
+      todayReceived: todayData.received,
+      todaySent: todayData.sent,
+      todayTotal: todayData.total,
+    });
   } catch (e) {
     console.error(e);
   }
+});
+
+app.listen(8083, async () => {
+  console.log("VNSTAT Monitor Running");
 });
